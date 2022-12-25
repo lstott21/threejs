9541d2b7b924e6c50c3f12c81ca14f64ea1f3ef6
Lucas Stott <lstott@okstate.edu>
2022-12-24 18:57:17 -0600

    updated css

--- src/Diamond.js
+++ src/Diamond.js
@@ -3,11 +3,10 @@ import { Canvas, useLoader } from '@react-three/fiber'
 import {
   useGLTF,
   MeshRefractionMaterial,
-  CubeCamera,
   AccumulativeShadows,
   RandomizedLight,
   Environment,
-  OrbitControls
+  OrbitControls,
 } from '@react-three/drei'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import { RGBELoader } from 'three-stdlib'
@@ -18,33 +17,25 @@ function Diamond(props) {
   // Use a custom envmap/scene-backdrop for the diamond material
   // This way we can have a clear BG while cube-cam can still film other objects
   const texture = useLoader(RGBELoader, 'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/aerodynamics_workshop_1k.hdr')
-  // Optional config
-  // const config = useControls({
-  //   bounces: { value: 4, min: 0, max: 8, step: 1 },
-  //   aberrationStrength: { value: 0.01, min: 0, max: 0.1, step: 0.01 },
-  //   ior: { value: 2.4, min: 0, max: 10 },
-  //   fresnel: { value: 1, min: 0, max: 1 },
-  //   color: 'white',
-  //   fastChroma: true
-  // })
   const config = {
-    bounces: 8,
-    aberrationStrength: .1,
-    ior: 8,
+    bounces: 5,
+    aberrationStrength: .05,
+    ior: 6,
     fresnel: 1,
     color: 'hotpink',
     fastChroma: true
   }
   return (
-    <CubeCamera resolution={512} frames={1} envMap={texture}>
-      {(texture) => (
         <mesh castShadow ref={ref} geometry={nodes.Diamond_1_0.geometry} {...props}>
           <MeshRefractionMaterial envMap={texture} {...config} toneMapped={false} />
         </mesh>
-      )}
-    </CubeCamera>
-  );
+      )
 }
+
+// const texture = useLoader(RGBELoader, "/textures/royal_esplanade_1k.hdr")
+// return (
+//   <mesh geometry={diamondGeometry} {...props}>
+//     <RefractionMaterial envMap={texture} />
 
 
 
@@ -52,28 +43,26 @@ export default function App() {
   return (
     <>
     <section className='App-header'>
-      <Canvas shadows camera={{ position: [-2, 1, 3], fov: 45 }}>
+      <Canvas shadows camera={{ position: [1, 1, 2.5], fov: 45}}>
         <color attach="background" args={['white']} />
-        <ambientLight intensity={0.5} color={0x404040} />
-        <spotLight position={[5, 5, -10]} angle={0.1} penumbra={5} />
-        <pointLight position={[-5, -5, -5]} color={0x404040} intensity={.5} />
         <Diamond position={[0, 0, 0]} />
         <AccumulativeShadows
           temporal
           frames={100}
-          color='black'
-          colorBlend={10}
+          color='white'
+          colorBlend={2}
           toneMapped={true}
-          alphaTest={0.8}
-          opacity={1}
-          scale={12}
-          position={[0, -0.5, 0]}>
-          <RandomizedLight amount={1} radius={10} ambient={0.5} intensity={.1} position={[5, 5, -10]} bias={0.01} />
+          alphaTest={0.9}
+          opacity={.75}
+          scale={10}
+          position={[0, -.45, 0]}
+          >
+          <RandomizedLight amount={8} radius={4} ambient={0.5} intensity={1} position={[5, 5, -10]} bias={0.01} />
         </AccumulativeShadows>
         <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/aerodynamics_workshop_1k.hdr" />
         <OrbitControls makeDefault autoRotate autoRotateSpeed={0.2} minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
         <EffectComposer>
-          <Bloom luminanceThreshold={2} intensity={1} levels={9} mipmapBlur />
+          <Bloom luminanceThreshold={1.75} intensity={1.5} levels={3} mipmapBlur />
         </EffectComposer>
       </Canvas>
     </section>
